36fedcbba5b79fec52cc9c9aab9ad0dd69d7ee26
mahimamadhav <gstmadhav@gmail.com>
2023-11-28 16:46:11 +0530

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA51_CA_Project_Team10.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(30)]
+        public string Username { get; set; }
+        [Required, MaxLength(100)]
+        public string Password { get; set; }
+        [Required, MaxLength(50)]
+        public string Salt { get; set; }
+        public virtual ICollection<Rating> Ratings { get; set; }
+    }
+}
